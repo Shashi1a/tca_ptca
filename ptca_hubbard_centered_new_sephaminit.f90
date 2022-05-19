4ebeea5b4238fae070a93218f68bc7124b065fca
shashikant <skunwar1a@gmail.com>
2022-05-19 10:36:45 +0530

    send/receive only updated variables

--- ptca_hubbard_centered_new_sephaminit.f90
+++ ptca_hubbard_centered_new_sephaminit.f90
@@ -12,24 +12,24 @@ program ptca_repulsive
   integer :: num_procs
   integer(8) :: site_clster,loc_proc
   real(8) :: tvar,rnum !! variable used to store intermediate temperature
-  integer(8),parameter :: L = 10 !! system size
+  integer(8),parameter :: L = 4 !! system size
   integer(8),parameter :: n_sites = L * L !! number of sites in the lattice
-  integer(8),parameter :: cls_sites =  8 !! cluster size
+  integer(8),parameter :: cls_sites =  2 !! cluster size
   integer(8),parameter :: ncl_by2 = 0.5*(cls_sites)+1 !! dividing cls_sites by 2
   integer(8),parameter :: n_splits = (ncl_by2)*(ncl_by2)
   integer(8),parameter :: split_sites = n_sites/n_splits
   integer(8),parameter :: cls_dim = (cls_sites)*(cls_sites) !! number of sites in the cluster
-  integer(8),parameter :: n_equil  = 2000 !! no of equilibrium steps
-  integer(8),parameter :: n_meas  = 2000 !! no of measurements
+  integer(8),parameter :: n_equil  = 1 !! no of equilibrium steps
+  integer(8),parameter :: n_meas  = 1 !! no of measurements
   integer(8),parameter :: meas_skip = 10 ! make measurement after this mc cycles
   integer(8),parameter :: dim_h = 2*n_sites  ! dimensionality of hamiltonian
   integer(8),parameter :: dim_clsh = 2*cls_dim ! dimensionality of cluster hamiltonian
   real(8),parameter :: temp = 0.30  !! simulation temperature
-  real(8),parameter :: dtemp = 0.01 !! temperature step to lower the temperature
+  real(8),parameter :: dtemp = 0.30 !! temperature step to lower the temperature
   real(8),parameter :: t_min = 0.01 !! minimum temperature for the simulation
   real(8),parameter :: pi = 4*atan(1.0)
   real(8),parameter :: t_hopping = 1.0
-  real(8),parameter :: u_int = 2.0
+  real(8),parameter :: u_int = 0.0
   real(8),parameter :: mu = 0.5*(u_int)
   real(8),parameter :: m_max=2.0_8,m_min=0.0_8
   real :: t_strt_equil, t_end_equil
